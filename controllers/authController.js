58329cbafc5d516ddc9cde4e6ee63d61da1e3dde
Torab Ramin <torabramin@gmail.com>
2019-12-01 04:05:04 +0600

    Added heroku config

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,18 +12,16 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
-  };
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
 
   // Remove some fields from output;
   user.password = undefined;
@@ -52,7 +50,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -71,7 +69,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // if everything ok, send token to user
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -225,7 +223,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // Update changed changedPasswordAt property for the user
   // Log the user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -244,5 +242,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // 4) login user, send JWT token
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
